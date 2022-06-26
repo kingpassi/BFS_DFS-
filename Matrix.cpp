71bc77d0cde500cef1a6cb2b7883ca9dfef818ac
YASH PASSI <98830689+kingpassi@users.noreply.github.com>
2022-06-26 23:19:14 +0530

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -14,63 +14,55 @@ Input: mat = [[0,0,0],[0,1,0],[1,1,1]]
 Output: [[0,0,0],[0,1,0],[1,2,1]]
 *******************************************************************************/
 class Solution {
-private:
-    map<pair<int,int>,int> vis;
-    map<pair<int,int>,int> lev;
-    vector <pair<int, int>> movements;
 public:
-    Solution() {
-        movements = {
-        {0,1}, {0,-1}, {1,0}, {-1,0},
-        };
-    }
-
-    bool isValid(int i, int j, int n, int m) {
-        return i>=0 && j>=0 && i<n && j<m;
-    }
-    void mutisource_bfs(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        queue<pair<int,int>> q;
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(grid[i][j]==0) {
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        queue<pair<int,int>>q;
+        vector<vector<int>>ans(mat.size(),vector<int>(mat[0].size(),-1));
+        
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[0].size();j++){
+                if(mat[i][j]==0){
                     q.push({i,j});
-                    lev[{i,j}]=0;
-                    vis[{i,j}]=1;
+                    ans[i][j]=0;
                 }
             }
         }
-        while(!q.empty()) {
-            auto v = q.front();
-            int v_x= v.first;
-            int v_y=v.second;
+      int  m=mat.size();
+        int n=mat[n].size();
+        
+        while(!q.empty()){
+            int i=q.front().first;
+            int j=q.front().second;
+            
+            if(checkvalid(i+1,j,m,n) && ans[i+1][j]==-1)
+            {
+                q.push({i+1,j});
+                ans[i+1][j]=ans[i][j]+1;
+            }
+             if(checkvalid(i-1,j,m,n) && ans[i-1][j]==-1)
+            {
+              q.push({i-1,j});
+                ans[i-1][j]=ans[i][j]+1;   
+            }
+             if(checkvalid(i,j+1,m,n) && ans[i][j+1]==-1)
+            {
+                 q.push({i,j+1});
+                ans[i][j+1]=ans[i][j]+1;
+            }
+             if(checkvalid(i,j-1,m,n) && ans[i][j-1]==-1)
+            {
+                q.push({i,j-1});
+                ans[i][j-1]=ans[i][j]+1;
+            }
             q.pop();
-
-            for(auto movement : movements) {
-                int child_x = movement.first+v_x;
-                int child_y = movement.second+v_y;
-                if(!isValid(child_x,child_y, n, m)) continue;
-                if(vis[{child_x,child_y}]) continue;
-                q.push({child_x,child_y});
-                lev[{child_x,child_y}] = lev[{v_x,v_y}]+1;
-                vis[{child_x,child_y}] = 1;
-            }
+            
         }
+         return ans;
     }
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        mutisource_bfs(mat);
-        int n = mat.size();
-        int m = mat[0].size();
-        vector <int> index;
-        vector<vector<int>> ans;
-        for(int i=0; i<n; ++i) {
-            for(int j=0; j<m; ++j) {
-                index.push_back(lev[{i,j}]);
-            }
-            ans.push_back(index);
-            index.clear();
-        }
-        return ans;
+    
+    bool checkvalid(int i,int j,int m,int n){
+        if(i<0||j<0||i>=m||j>=n) return false;
+        return true;
     }
+   
 };
